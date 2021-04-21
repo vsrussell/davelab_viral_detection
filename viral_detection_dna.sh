8168b7e40faa29e2dd558d6f0b931a90b39005d5
Veronica Russell <vronrussell@hotmail.com>
2021-04-21 11:03:37 -0400

    add viral_detection_dna.sh for easy docker download

---
+++ viral_detection_dna.sh
@@ -0,0 +1,27 @@
+#!usr/bin/sh
+#Veronica Russell
+#04/20/21
+
+#viral detecton for viruses of interest (EBV, HIV, KSHV, HPV, HCV, HBV, HTLV, MPV) from sample dna sequencing 
+
+IN_BAM=$1 #pcr marked duplicates file from gs://analysis_results/${ANALYSIS_ID}/filter_chrom_reads__short_insert__dna/filter_chrom_reads__short_insert__dna_FilterChromReads_FilterLongInsertExonicReads.${DAVELAB_ID}.small_insert.bam
+REF_IDX=$2 #intended use: Viral masked reference from VirDetect workflow index with bwa index
+NTHREADS=$3 #nr_cpus
+INCLUDE_FLAG=$4
+EXCLUDE_FLAG=$5
+OUT_PREF_PAIR=$6 #ouput naming
+
+#filter input bam and convert to fastqs
+samtools fastq -f ${INCLUDE_FLAG} -F ${EXCLUDE_FLAG} ${IN_BAM} -1 human_unmapped_dna_to_masked_viral_BWA1.fastq -2 human_unmapped_dna_to_masked_viral_BWA2.fastq -s /dev/null
+
+#run bwa
+bwa mem -t ${NTHREADS} -M ${REF_IDX} human_unmapped_dna_to_masked_viral_BWA1.fastq human_unmapped_dna_to_masked_viral_BWA2.fastq > ${OUT_PREF_PAIR}_aligned.sam
+
+
+#sort and index for idxstats
+samtools view -b ${OUT_PREF_PAIR}_aligned.sam | samtools sort - > ${OUT_PREF_PAIR}_aligned_sorted.bam
+samtools index ${OUT_PREF_PAIR}_aligned_sorted.bam
+samtools idxstats ${OUT_PREF_PAIR}_aligned_sorted.bam > ${OUT_PREF_PAIR}_idxstats.txt
+
+ls -ltr
+ls -ltr /data/viral_detection_dna/output/
