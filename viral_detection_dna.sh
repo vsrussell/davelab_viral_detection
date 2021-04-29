641cd48324f05a66ca3ac719d970565e77147adf
Veronica Russell <vronrussell@hotmail.com>
2021-04-29 17:29:56 -0400

    updating output log file naming viral_detection_rna.sh and viral_detection_dna.sh

--- viral_detection_dna.sh
+++ viral_detection_dna.sh
@@ -12,16 +12,16 @@ EXCLUDE_FLAG=$5
 OUT_PREF_PAIR=$6 #ouput naming
 
 #filter input bam and convert to fastqs
-samtools fastq -f ${INCLUDE_FLAG} -F ${EXCLUDE_FLAG} ${IN_BAM} -1 human_unmapped_dna_to_masked_viral_BWA1.fastq -2 human_unmapped_dna_to_masked_viral_BWA2.fastq -s /dev/null 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
+samtools fastq -f ${INCLUDE_FLAG} -F ${EXCLUDE_FLAG} ${IN_BAM} -1 human_unmapped_dna_to_masked_viral_BWA1.fastq -2 human_unmapped_dna_to_masked_viral_BWA2.fastq -s /dev/null 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
 
 #run bwa
-bwa mem -t ${NTHREADS} -M ${REF_IDX} human_unmapped_dna_to_masked_viral_BWA1.fastq human_unmapped_dna_to_masked_viral_BWA2.fastq -o ${OUT_PREF_PAIR}_aligned.sam 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
+bwa mem -t ${NTHREADS} -M ${REF_IDX} human_unmapped_dna_to_masked_viral_BWA1.fastq human_unmapped_dna_to_masked_viral_BWA2.fastq -o ${OUT_PREF_PAIR}_aligned.sam 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
 
 
 #sort and index for idxstats
-samtools view -b ${OUT_PREF_PAIR}_aligned.sam | samtools sort - > ${OUT_PREF_PAIR}_aligned_sorted.bam 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
-samtools index ${OUT_PREF_PAIR}_aligned_sorted.bam 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
-samtools idxstats ${OUT_PREF_PAIR}_aligned_sorted.bam > ${OUT_PREF_PAIR}_idxstats.txt 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
+samtools view -b ${OUT_PREF_PAIR}_aligned.sam | samtools sort - > ${OUT_PREF_PAIR}_aligned_sorted.bam 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
+samtools index ${OUT_PREF_PAIR}_aligned_sorted.bam 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
+samtools idxstats ${OUT_PREF_PAIR}_aligned_sorted.bam > ${OUT_PREF_PAIR}_idxstats.txt 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
 
-ls -ltr 2>&1 | tee -a ${OUT_PREF_PAIR}_log.txt
+ls -ltr 2>&1 | tee -a ${OUT_PREF_PAIR}_all_log.txt
 ls -ltr /data/viral_detection/output/
